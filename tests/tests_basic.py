152b677cf183e7ea5938083aabe5267a426cb039
Pedro Chazarra <pedrochgb@gmail.com>
2021-04-16 14:00:13 +0200

    Adding some additional tests

--- tests/tests_basic.py
+++ tests/tests_basic.py
@@ -1,11 +1,16 @@
 import unittest
-from eegraph.graph import *
+from eegraph.graph import Graph
 from eegraph.importData import *
 from eegraph.modelateData import *
 from eegraph.strategy import *
 from eegraph.tools import *
 
 class TestTools(unittest.TestCase):
+    connectivity_measures = {'cross_correlation': 'Cross_correlation_Estimator', 'pearson_correlation': 'Pearson_correlation_Estimator', 'squared_coherence': 'Squared_coherence_Estimator',
+                         'imag_coherence': 'Imag_coherence_Estimator', 'corr_cross_correlation': 'Corr_cross_correlation_Estimator', 'wpli': 'Wpli_Estimator', 
+                         'plv': 'Plv_Estimator', 'pli': 'Pli_No_Bands_Estimator', 'pli_bands': 'Pli_Bands_Estimator', 'dtf': 'Dtf_Estimator', 'power_spectrum': 'Power_spectrum_Estimator',
+                         'spectral_entropy': 'Spectral_entropy_Estimator', 'shannon_entropy': 'Shannon_entropy_Estimator'}
+    
     def test_processed_input_bands(self):
         frequency_bands = ['delta', 'thet', 'alpha', 'betah', 'gamma']
         expexted_result = [True, False, True, False, True]
@@ -43,9 +48,10 @@ class TestTools(unittest.TestCase):
         result = calculate_time_intervals(data, sample_rate, sample_duration, seconds, sample_length)
         
         for i, segment in enumerate(result[0]):
+            print('seg1', segment)
             self.assertEqual(segment, expected_result_data[i])
-            self.assertEqual(result[1], expected_interval)
-            self.assertEqual(result[2], expected_flag)
+        self.assertEqual(result[1], expected_interval)
+        self.assertEqual(result[2], expected_flag)
         
     def test_calculate_intervals_flag(self):
         data = ([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]])
@@ -64,13 +70,49 @@ class TestTools(unittest.TestCase):
         self.assertEqual(result[1], expected_interval)
         self.assertEqual(result[2], expected_flag)
         
+    def test_calculate_intervals_list_Exception_exceed_sample_length(self):
+        data = ([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]])
+        sample_rate = 10
+        sample_duration = 2.5
+        seconds = [0,3]
+        sample_length = 29
+        
+        with self.assertRaises(Exception):
+            calculate_time_intervals(data, sample_rate, sample_duration, seconds, sample_length)
+            
+            
+    def test_calculate_intervals_list_Exception_intervals_not_starting_0(self):
+        data = ([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]])
+        sample_rate = 10
+        sample_duration = 3
+        seconds = [1, 2]
+        sample_length = 29
+        
+        with self.assertRaises(Exception):
+            calculate_time_intervals(data, sample_rate, sample_duration, seconds, sample_length)
+        
     def test_calculate_bands_fft(self):
-        values = np.random.uniform(0, 100, 8192)
+        values = np.random.uniform(0, 100, 2048)
         sample_rate = 512 
         bands= [True, False, True, False, True]
         result = calculate_bands_fft(values, sample_rate, bands)
         
-        self.assertTrue(result[0].mean() > result[1].mean() > result[2].mean())
+        self.assertTrue((result[0].mean() >= result[1].mean()) and (result[0].mean() >=  result[2].mean()))
+        
+    def test_search_method(self):
+        connectivity = 'cross_correlation'
+        expected_result = 'Cross_correlation_Estimator()'
+        
+        result = search(connectivity_measures, connectivity)
+        self.assertEqual(result, expected_result)
     
+    def test_search_method_NameError(self):     
+        connectivity = 'cross_correlations'
+        with self.assertRaises(NameError):
+            search(connectivity_measures, connectivity)
+        
+    
+        
+        
 if __name__ == '__main__':
     unittest.main()
