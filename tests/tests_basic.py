9cb9545a59be561191bd00e131fe2ae6d47fc27d
Pedro Chazarra <pedrochgb@gmail.com>
2021-04-19 11:01:21 +0200

    Updating tests

--- tests/tests_basic.py
+++ tests/tests_basic.py
@@ -16,6 +16,9 @@ class TestTools(unittest.TestCase):
         expexted_result = [True, False, True, False, True]
         result = input_bands(frequency_bands)
         self.assertEqual(result, expexted_result)
+        
+    #=================    
+    #Channel names
     
     def test_processed_channel_names_space(self):
         channel_names = ['EEG Fp1', 'EEG Fp2', 'EEG AF7', 'EEG AF3', 'EEG AF4', 'EEG AF8', 'EEG F7', 'EEG F5', 'EEG F3', 'EEG F1', 'EEG Fz', 'EEG F2', 'EEG F4', 'EEG F6']
@@ -35,10 +38,14 @@ class TestTools(unittest.TestCase):
         result = process_channel_names(channel_names)
         self.assertEqual(result, expected_channel_names)
         
-    def test_calculate_intervals_no_flag(self):
+    
+    #=================    
+    #Time intervals
+    
+    def test_calculate_intervals_float_no_flag(self):
         data = ([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]])
         sample_rate = 2
-        sample_duration = 7.5
+        sample_duration = np.float64(7.5)
         seconds = 2
         sample_length = 15
         expected_result_data = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15]]
@@ -53,10 +60,10 @@ class TestTools(unittest.TestCase):
         self.assertEqual(result[1], expected_interval)
         self.assertEqual(result[2], expected_flag)
         
-    def test_calculate_intervals_flag(self):
+    def test_calculate_intervals_float_flag(self):
         data = ([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]])
         sample_rate = 10
-        sample_duration = 2.9
+        sample_duration = np.float64(2.9)
         seconds = 3
         sample_length = 29
         expected_result_data = [[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29], [1,2,3,4,5,6,7,8,9,10]]
@@ -70,10 +77,12 @@ class TestTools(unittest.TestCase):
         self.assertEqual(result[1], expected_interval)
         self.assertEqual(result[2], expected_flag)
         
+       
+        
     def test_calculate_intervals_list_Exception_exceed_sample_length(self):
         data = ([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]])
         sample_rate = 10
-        sample_duration = np.float64(3)
+        sample_duration = np.float64(2.5)
         seconds = [0,3]
         sample_length = 29
         
@@ -90,7 +99,10 @@ class TestTools(unittest.TestCase):
         
         with self.assertRaises(Exception):
             calculate_time_intervals(data, sample_rate, sample_duration, seconds, sample_length)
-        
+            
+    #=================    
+    #Frequency bands 
+    
     def test_calculate_bands_fft(self):
         values = np.random.uniform(0, 100, 2048)
         sample_rate = 512 
