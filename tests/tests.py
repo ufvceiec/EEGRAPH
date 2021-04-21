585a9891172e811d655648285f8764e6109892c7
Pedro Chazarra <pedrochgb@gmail.com>
2021-04-21 11:01:11 +0200

    Updating tests

--- tests/tests.py
+++ tests/tests.py
@@ -323,7 +323,7 @@ class TestImportData(unittest.TestCase):
         
     def test_load_data_electrode_montage(self):
         path = 'tests/Test_files/test_eeg.gdf'                               
-        electrode_montage_path = "Test_files/electrodemontage.set.ced"
+        electrode_montage_path = "tests/Test_files/electrodemontage.set.ced"
         expected_ch_names = ['Fp1', 'Fp2', 'AF7', 'AF3', 'AF4', 'AF8', 'F7', 'F5', 'F3', 'F1', 'Fz', 'F2', 'F4', 'F6', 'F8', 'FT9', 'FT7', 'FC5', 'FC3', 'FC1', 'FCz', 'FC2', 'FC4', 'FC6', 'FT8', 'FT10', 'T7', 
                              'C5', 'C3', 'C1', 'Cz', 'C2', 'C4', 'C6', 'T8', 'TP9', 'TP7', 'CP5', 'CP3', 'CP1', 'CPz', 'CP2', 'CP4', 'CP6', 'TP8', 'TP10', 'P7', 'P5', 'P3', 'P1', 'Pz', 'P2', 'P4', 'P6', 'P8', 
                              'PO7', 'PO3', 'POz', 'PO4', 'PO8', 'PO9', 'O1', 'Oz', 'O2']         #Labels in electrode montage file
