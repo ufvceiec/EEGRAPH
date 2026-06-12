7ec1677c39cd1b5686e3083427d294b72d5451e8
Alberto Nogales <albertonogales@MacBook-Air-de-Alberto-2.local>
2026-06-12 20:54:24 +0200

    Bump version to 0.1.17

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
 setup(
     name='EEGRAPH',
-    version='0.1.16',
+    version='0.1.17',
     description='',
     url='https://github.com/ufvceiec/EEGRAPH',
     author='CEIEC',
